ba85b50a7a38859a6fd8dd005dc18df80916d988
aggelladadcs <aggellada@up.edu.ph>
2025-06-25 15:59:10 +0800

    -fixed bugs

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,21 +5,25 @@ import cors from "cors";
 
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
-import { connectDb } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-
 const __dirname = path.resolve();
 
-app.use(express.json()); // allows to extract json data out of req.body
-app.use(cookieParser()); // allow you to parse/read the data of the cookie
-app.use(cors({ origin: "http://localhost:5173", credentials: true })); // different localhost frontend and backend
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
@@ -27,12 +31,12 @@ app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log("Server running on Port", PORT + ".");
-  connectDb();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
